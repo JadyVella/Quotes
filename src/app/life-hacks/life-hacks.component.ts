2e0b2771af593a1119675616e51442c85616fb70
JadyVella <felixouma379@gmail.com>
2020-01-14 10:53:52 +0300

    Add a fifth quote in life-hacks.component.ts

--- src/app/life-hacks/life-hacks.component.ts
+++ src/app/life-hacks/life-hacks.component.ts
@@ -13,8 +13,8 @@ export class LifeHacksComponent implements OnInit {
     new LifeHacks(1, "if you want to walk fast, walk alone. If you want to walk far walk together", "Jady", "Felix", new Date(2020,1,12)),
     new LifeHacks(2, "Give without remembering and receive without forgeting", "Jady", "Felix", new Date(2020,1,12)),
     new LifeHacks(3, "The best things in life are free", "Jady", "Felix", new Date(2020,1,12)),
-    // new LifeHacks(4, "The best is yet to come", "Jady", "Felix", new Date(2020,1,12)),
-    new LifeHacks(5, "Hard doesn't mean impossible", "Jady", "Felix", new Date(2020,1,12)),
+    new LifeHacks(4, "The best is yet to come", "Jady", "Felix", new Date(2020,1,12)),
+    // new LifeHacks(5, "Hard doesn't mean impossible", "Jady", "Felix", new Date(2020,1,12)),
 
   ];
 
