68ee998861facbd0cf219f11805e9e6924bdb75d
JadyVella <felixouma379@gmail.com>
2020-01-14 10:49:36 +0300

    Import class LifeHacks to life-hacks.components.ts

--- src/app/life-hacks/life-hacks.component.ts
+++ src/app/life-hacks/life-hacks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LifeHacks } from '../life-hacks'
+// import { LifeHacks } from '../life-hacks'
 
 @Component({
   selector: 'app-life-hacks',
