706bd535aa2a1d28670562cf8ccf0c1be1447124
JadyVella <felixouma379@gmail.com>
2020-01-14 10:51:08 +0300

    Add a first quote in life-hacks.component.ts

--- src/app/life-hacks/life-hacks.component.ts
+++ src/app/life-hacks/life-hacks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { LifeHacks } from '../life-hacks'
+import { LifeHacks } from '../life-hacks'
 
 @Component({
   selector: 'app-life-hacks',
@@ -10,7 +10,7 @@ export class LifeHacksComponent implements OnInit {
 
 
   lifehacks: LifeHacks[] = [
-    new LifeHacks(1, "if you want to walk fast, walk alone. If you want to walk far walk together", "Jady", "Felix", new Date(2020,1,12)),
+    // new LifeHacks(1, "if you want to walk fast, walk alone. If you want to walk far walk together", "Jady", "Felix", new Date(2020,1,12)),
     new LifeHacks(2, "Give without remembering and receive without forgeting", "Jady", "Felix", new Date(2020,1,12)),
     new LifeHacks(3, "The best things in life are free", "Jady", "Felix", new Date(2020,1,12)),
     new LifeHacks(4, "The best is yet to come", "Jady", "Felix", new Date(2020,1,12)),
